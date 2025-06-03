ffccf206c6c04936b4a6b5240033af76db191f7a
admgk <adamgorz@gmail.com>
2025-06-03 22:30:40 +0200

    Change subtract screen horizontal grid look

--- app/src/main/java/com/ignismark/chronotool/ui/screens/SubtractScreen.kt
+++ app/src/main/java/com/ignismark/chronotool/ui/screens/SubtractScreen.kt
@@ -1,10 +1,15 @@
 package com.ignismark.chronotool.ui.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.material3.Card
@@ -16,7 +21,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -124,15 +131,31 @@ fun SubtractScreen(
             HorizontalDivider()
 
             LazyHorizontalGrid(
-                modifier = Modifier.fillMaxWidth().height(96.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(96.dp),
                 rows = GridCells.Fixed(3),
                 userScrollEnabled = true,
                 horizontalArrangement = Arrangement.Start,
                 verticalArrangement = Arrangement.Top,
             ) {
                 items(uiState.valuesList.size) { index ->
-                    Card {
-                        Text(text = uiState.valuesList[index].toString())
+                    Card(
+                        modifier = Modifier
+                            .aspectRatio(3f)
+                            .padding(2.dp),
+                        border = BorderStroke(1.dp, Color.Black)
+                    ) {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = uiState
+                                .valuesList[index]
+                                .toComponents { hours, minutes, seconds, nanoseconds ->
+                                "${hours}H ${minutes}M ${seconds}S" }
+                            )
+                        }
                     }
                 }
             }
