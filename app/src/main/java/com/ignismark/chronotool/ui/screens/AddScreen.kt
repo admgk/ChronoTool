a1dd4694ac3cbd3ce81f10c7e304f5c6ec78d835
admgk <adamgorz@gmail.com>
2025-07-02 20:06:42 +0200

    refactor: optimize imports

--- app/src/main/java/com/ignismark/chronotool/ui/screens/AddScreen.kt
+++ app/src/main/java/com/ignismark/chronotool/ui/screens/AddScreen.kt
@@ -3,9 +3,7 @@ package com.ignismark.chronotool.ui.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
--- app/src/main/java/com/ignismark/chronotool/ui/screens/SubtractScreen.kt
+++ app/src/main/java/com/ignismark/chronotool/ui/screens/SubtractScreen.kt
@@ -1,14 +1,9 @@
 package com.ignismark.chronotool.ui.screens
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
