1e41c0b3ffcfa11a400ec95c3b327ba146614226
admgk <adamgorz@gmail.com>
2025-07-02 19:15:10 +0200

    feat: specify singleLine and KeyboardType.Number in InputForm

--- app/src/main/java/com/ignismark/chronotool/ui/components/InputForm.kt
+++ app/src/main/java/com/ignismark/chronotool/ui/components/InputForm.kt
@@ -1,9 +1,11 @@
 package com.ignismark.chronotool.ui.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.input.KeyboardType
 
 @Composable
 fun InputForm(
@@ -21,21 +23,27 @@ fun InputForm(
             onValueChange = onChangeHours,
             label = {
                 Text(text = "Hours")
-            }
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
         OutlinedTextField(
             value = minutes,
             onValueChange = onChangeMinutes,
             label = {
                 Text(text = "Minutes")
-            }
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
         OutlinedTextField(
             value = seconds,
             onValueChange = onChangeSeconds,
             label = {
                 Text(text = "Seconds")
-            }
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
     }
 }
