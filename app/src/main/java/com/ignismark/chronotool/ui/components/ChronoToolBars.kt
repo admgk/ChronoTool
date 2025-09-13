8e6f87429418f40f7f26b7ab999cbdc886250092
admgk <adamgorz@gmail.com>
2025-09-13 18:09:20 +0200

    refactor: remove unused imports in ChronoToolBars.kt

--- app/src/main/java/com/ignismark/chronotool/ui/components/ChronoToolBars.kt
+++ app/src/main/java/com/ignismark/chronotool/ui/components/ChronoToolBars.kt
@@ -2,18 +2,8 @@ package com.ignismark.chronotool.ui.components
 
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
-import androidx.navigation.NavController
-import com.ignismark.chronotool.R
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.compose.runtime.getValue
-import com.ignismark.chronotool.ui.utils.ChronoToolRoutes
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
